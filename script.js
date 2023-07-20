3fa53623b2fcc0f5322c0a8675ea23a0f04d6f1b
jayasankar J P <scicopath01@gmail.com>
2023-07-20 17:13:21 +0530

    update The Envionment

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     let inputObj=document.getElementById("data_block")
     let ourList =document.getElementById("list-ul")
-    let finBut=document.getElementById("finishWork")
     let markItom=document.getElementById("Mark")
-    finBut.style.display="none"
     oruForm.addEventListener("submit",(e) => {
         e.preventDefault()
         console.log(inputObj.value)
@@ -15,38 +13,44 @@ document.addEventListener("DOMContentLoaded",()=>{
     })
     function createItom(x){
         if(x!=""){
-        let MessageChannel=`<div><li>${x}<button onClick="deleteItom(this)" class="Finish_but">Complete</button></li><div>`
+        let MessageChannel=`<div class="task"><li><p>${x}</p><div onClick="finishItom(this)" class="Finish_but">&check;</div> <div class="Delete_but" id="intoMark" onClick= deleteItom(this)>&#10008;</div></li><div>`
             ourList.insertAdjacentHTML("beforeend",MessageChannel)
             inputObj.value=""
             total+=1;
-            if(total==0){
-                finBut.style.display="none"
-            }else{
-                finBut.style.display="block"
+            if(total!=0)
                 markItom.innerHTML=`${finish}/${total}`
-            }
-
         }
         inputObj.focus()
     }
 
     
 })
-function deleteItom(elementToDelete){
+function finishItom(elementToDelete){
      elementToDelete.parentElement.parentElement.classList.add("Finish_work")
-     elementToDelete.remove()
-     finish+=1
-     let markItom=document.getElementById("Mark")
-     markItom.innerHTML=`${finish}/${total}`
-    //alert("data")
+    // console.log(elementToDelete.innerHTML )
+    if(elementToDelete.innerHTML=="✓"){
+        // console.log("Iniside if finish Work")
+        elementToDelete.innerHTML="&nbsp;&nbsp;&nbsp;"
+        finish+=1
+        let markItom=document.getElementById("Mark")
+        markItom.innerHTML=`${finish}/${total}`
+    }
+     
 }
-function finishTask(){
-    let ourList =document.getElementById("list-ul")
-    let finBut=document.getElementById("finishWork")
-    ourList.innerHTML=''
-    total=0
-    finish=0
-    finBut.style.display="none"
+function deleteItom(elementToDelete){
+    // console.log(elementToDelete.parentElement.innerHTML)
+    
+        if(!elementToDelete.parentElement.innerHTML.includes('✓')){
+            finish-=1
+        }
+            // console.log("inisde If to delete")
+        // }else{
+        //     console.log("inisde else to delete")
+        // }
+    elementToDelete.parentElement.parentElement.remove() 
+    elementToDelete.remove()
+   
+    total-=1
     let markItom=document.getElementById("Mark")
-     markItom.innerHTML=""
+    markItom.innerHTML=`${finish}/${total}`
 }
